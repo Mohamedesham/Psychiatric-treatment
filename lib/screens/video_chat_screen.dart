ac26feb296a58531dbbbb915b1758e7e0d27670c
Mohamedesham <mohamed.hesham2642001@gmail.com>
2023-06-07 19:38:04 +0300

    last update 7/6/2023

---
+++ lib/screens/video_chat_screen.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+
+class VideoScreen extends StatelessWidget {
+   VideoScreen({Key? key,required this.callID}) : super(key: key);
+  final String callID;
+  String userID=Random().nextInt(10000).toString();
+
+  @override
+  Widget build(BuildContext context) {
+    return ZegoUIKitPrebuiltCall(
+        appID: 2060782974,
+        appSign: "yourAppSign",
+        callID: callID,
+        userID: userID,
+        userName: 'user_$userID',
+        config: ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall());
+  }
+}
